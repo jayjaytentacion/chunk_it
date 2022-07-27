d1c2df9c7a0ac5a3d65c33f75039b353070556c6
sydney <sydney.idundun@gmail.com>
2022-07-27 23:20:26 +0200

    working on the view for the dashboard file upload

--- api/models.py
+++ api/models.py
@@ -4,23 +4,11 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 
-<<<<<<< HEAD
-class UploadedFile(models.Model):
-    cutomuser=models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name='custom user')
-    file=models.FieldFile(upload_to="largefile")
-    fiename=models.CharField(max_length=20)
-
-
-class Chunks(models.Model):
-    uploadedflie=models.ForeignKey(UploadedFile,on_delete=models.CASCADE,related_name="uploadedflie")
-    chunk=models.FileField(upload_to="chunks")
-    chunksize=models.IntegerField()
-=======
 
 class UploadedFile(models.Model):
     cutomuser = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, related_name='custom user')
-    file = models.FieldFile(upload_to="largefile")
+        get_user_model(), on_delete=models.CASCADE, related_name='custom user')
+    file = models.FileField(upload_to="largefile")
     fiename = models.CharField(max_length=20)
 
 
@@ -29,4 +17,3 @@ class Chunks(models.Model):
         UploadedFile, on_delete=models.CASCADE, related_name="uploadedfile")
     chunk = models.FileField(upload_to="chunks")
     chunksize = models.IntegerField()
->>>>>>> 14c78b7f79d844def10601fc79017be444f528d2
