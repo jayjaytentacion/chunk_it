f86fa22b93bdc43a0145325a631672575cc3e629
sydney <sydney.idundun@gmail.com>
2022-07-28 17:36:10 +0200

    made some new changes

---
+++ api/utils.py
@@ -0,0 +1,13 @@
+# this file contains pure python logic for file validation 
+# file chunking and chunk zipping 
+import os
+from django.forms import ValidationError
+
+def ValidateFile(UploadedFile):
+    # this serves as validation for the Upload file form in django
+    file_name = str(UploadedFile)
+    if UploadedFile.name.endswith('.csv') or UploadedFile.name.endswith(".json"):
+        return UploadedFile
+    else:
+        raise ValidationError("Only CSV and JSON files are permitted")
+
